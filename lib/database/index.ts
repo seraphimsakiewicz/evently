7416613db52048a37cf59f0922399723117175af
Seraphim <seraphim.codes@gmail.com>
2024-01-11 20:44:24 -0500

    fixed db bug to return cached connection

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,21 +1,20 @@
-import mongoose from "mongoose"
+import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.MONGODB_URI;
 
-// attempt to receive a mongoose prop from global obj, holds cached connection to db
-let cached = (global as any).mongoose || { conn: null, promise: null }
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
-    if (cached.conn) return cached.conn
+    if (cached.conn) return cached.conn;
 
-    if (!MONGODB_URI) throw new Error("MONGODB_URI is missing")
+    if (!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
-    // connect already cached connection or make new connection
     cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-        dbName: "evently",
+        dbName: 'evently',
         bufferCommands: false,
     })
 
-    cached.conn = await cached.promise
+    cached.conn = await cached.promise;
 
+    return cached.conn;
 }
